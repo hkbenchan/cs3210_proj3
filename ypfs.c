9a021038589ba93ac2fa141c5b3974f1213eced5
Ben Chan <ustbenchan@gmail.com>
2012-12-04 16:20:38 -0500

    test

--- ypfs.c
+++ ypfs.c
@@ -1186,23 +1186,21 @@ void my_little_curl_test() {
 }
 
 // feedback from server
-size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
-	
+static size_t read_callback(void *ptr, size_t size, size_t nmemb, void *stream)
+{
 	size_t retcode;
-	  curl_off_t nread;
-
-	  /* in real-world cases, this would probably get this data differently
-	     as this fread() stuff is exactly what the library already would do
-	     by default internally */ 
-	  retcode = fread(ptr, size, nmemb, stream);
-
-	  nread = (curl_off_t)retcode;
-
-	  printf("*** We read %" CURL_FORMAT_CURL_OFF_T
-	          " bytes from file\n", nread);
-
-	  return retcode;
-	
+	curl_off_t nread;
+ 
+	/* in real-world cases, this would probably get this data differently
+	   as this fread() stuff is exactly what the library already would do
+	   by default internally */ 
+	retcode = fread(ptr, size, nmemb, stream);
+    nread = (curl_off_t)retcode;
+ 
+  	fprintf(stderr, "*** We read %" CURL_FORMAT_CURL_OFF_T
+	        " bytes from file\n", nread);
+ 
+	return retcode;
 }
 
 /*** end of curl testing ***/
