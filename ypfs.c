05e4429166216c0c4a2adf5e76854c3b2374acf6
Ben Chan <ustbenchan@gmail.com>
2012-12-04 16:38:31 -0500

    test

--- ypfs.c
+++ ypfs.c
@@ -1191,7 +1191,8 @@ void my_little_curl_test() {
 		abort();
 	}
 	printf("curl_easy_init\n");
-	curl_easy_setopt(curl_handler, CURLOPT_URL, "http://ec2-54-243-84-10.compute-1.amazonaws.com/cs4261_proj2_web/index.php/"); 
+	curl_easy_setopt(curl_handler, CURLOPT_URL, "http://ec2-54-243-84-10.compute-1.amazonaws.com/cs4261_proj2_web/index.php/register.json");
+	curl_easy_setopt(curl_handler, CURLOPT_POSTFIELDS, "Username=abcdefg"); 
 	//curl_easy_setopt(curl_handler, CURLOPT_WRITEFUNCTION, write_data); 
 	//curl_easy_setopt(curl_handler, CURLOPT_WRITEFUNCTION, read_callback); 
 	
