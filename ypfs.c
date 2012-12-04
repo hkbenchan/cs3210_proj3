3e5e8227e4ec74a654726cadba2efdbcaf9e1187
Ben Chan <ustbenchan@gmail.com>
2012-12-04 13:20:27 -0500

    test

--- ypfs.c
+++ ypfs.c
@@ -269,7 +269,7 @@ void remove_self_and_children_file(struct YP_NODE *parent) {
 		unlink(absolute_path);
 }
 
-void remove_temp_file() {
+void remove_copied_file() {
 	FSLog("Start removing...");
 	remove_self_and_children_file(root_node);
 	FSLog("Finish removing...");
