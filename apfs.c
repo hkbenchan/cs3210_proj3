c9e508151efcf7b858742e66e961eab73840fd9a
Ben Chan <ustbenchan@gmail.com>
2012-12-06 16:05:22 -0500

    test

--- apfs.c
+++ apfs.c
@@ -1471,9 +1471,11 @@ int main(int argc, char *argv[])
 		abort();
 	}
 	
-	if (stat("/tmp/apfs", stbuf) == 0) {
-		fprintf(stderr, "**********APFS folder does not exists\n");
-		mkdir("/tmp/apfs/", 00666);
+	if (stat("/tmp/apfs/", stbuf) == 0) {
+		int ret;
+		
+		ret = mkdir("/tmp/apfs/", 00666);
+		fprintf(stderr, "**********APFS folder does not exists, return %d\n", ret);
 		abort();
 	}
 	
