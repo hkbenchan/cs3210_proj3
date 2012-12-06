fc82239f5c816fe46757ec405fab793a327f7ce5
Ben Chan <ustbenchan@gmail.com>
2012-12-05 22:34:41 -0500

    update

--- ypfs.c
+++ ypfs.c
@@ -1436,7 +1436,7 @@ static size_t normal_callback(char *ptr, size_t size, size_t nmemb, void *stream
 {
 	size_t retcode;
 	curl_off_t nread;
- 
+	fprintf(stderr, "*********Normal callback\n");
 	/* in real-world cases, this would probably get this data differently
 	   as this fread() stuff is exactly what the library already would do
 	   by default internally */ 
