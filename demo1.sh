dd5576b1cc9cdeb8290ba2e29f33a768a4d9b767
Ben Chan <ustbenchan@gmail.com>
2012-12-06 16:11:22 -0500

    final update

--- demo1.sh
+++ demo1.sh
@@ -1,4 +1,12 @@
-for i in {1..10}
+date +%s%N
+for i in {1..20}
 do
-	echo $((i)).abc
+# cp pics/logo_inst210.gif temp/logo_$((i)).gif # step 1, 2, 3
+# cp pics/logo_inst210.gif temp/logo_$((i)).gif+private # step 4a
+# cp temp/2012/December/logo_$((i)).gif+private temp/logo_$((i)).gif # step 4b
+# cp pics/Cherry002.jpg temp/Cherry002_$((i)).jpg # step 5, 6
+# cp pics/Cherry002.jpg temp/Cherry002_$((i)).jpg+private #step 7a
+# cp temp/2012/December/Cherry002_$((i)).jpg+private temp/Cherry002_$((i)).jpg #step 7b
+
 done
+date +%s%N
